7a7adfdfa78e996e2474f63ca0a5a1239fdbc7ef
Saadmamdouh123 <saadmamdouh6@gmail.com>
2025-07-07 12:34:29 +0100

    add last task

--- Pand.py
+++ Pand.py
@@ -7,6 +7,7 @@ data = {
 }
 
 df = pd.DataFrame(data)
+print(df)
 # print(df.head())
 # print(df.tail(2))
 # print(df.info())
@@ -24,4 +25,11 @@ df = pd.DataFrame(data)
 # #print(df.isnull())
 # replace = df.fillna(df["Age"].mean())
 # print(replace)
+sorted_df = df.sort_values(by='Age', ascending=False)
+# print(sorted_df)
+
+new_df = df.drop('Age', axis='columns')
+# print(new_df) 
+new_df2 = df.drop(index=1)
+print(new_df2)
 
